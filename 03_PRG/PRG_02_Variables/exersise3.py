28edea9b7a2c8004cf4466f5452e3a372baac674
Marcel Koek <>
2023-08-14 17:54:15 +0200

    [added block comments with the assignment at the top]

--- 03_PRG/PRG_02_Variables/exersise3.py
+++ 03_PRG/PRG_02_Variables/exersise3.py
@@ -1,3 +1,13 @@
+"""
+Create a new script.
+Create a variable and assign a value to it.
+Print the text “Value 1: <value 1 here>”.
+Change the value of that same variable.
+Print the text “Value 2: <value 2 here>”.
+Change the value of that same variable.
+Print the text “Value 3: <value 3 here>
+"""
+
 value = 23
 print(f"Value 1: {value}")
 value = "marcel"
