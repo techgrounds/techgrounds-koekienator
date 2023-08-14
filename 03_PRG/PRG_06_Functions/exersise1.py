d2df9c12c4f53cf5e1875b2f8fee0d765ffd8c8e
Marcel Koek <>
2023-08-14 12:00:10 +0200

    [added functions assignment]

---
+++ 03_PRG/PRG_06_Functions/exersise1.py
@@ -0,0 +1,11 @@
+"""
+Create a new script.
+Import the random package.
+Print 5 random integers with a value between 0 and 100.
+"""
+import random
+
+# loop 5 times
+for i in range(5):
+    # Generate a random int 0 - 100
+    print(random.randint(0,101))
