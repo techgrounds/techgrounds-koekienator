9b60d87b21e20fc349bd94c69bfef755780ef48a
Marcel Koek <>
2023-08-14 11:59:03 +0200

    [added loops assignements]

---
+++ 03_PRG/PRG_04_Loops/exersise3.py
@@ -0,0 +1,14 @@
+"""
+Create a new script.
+Copy the array below into your script.
+arr = ["Coen", "Casper", "Joshua", "Abdessamad", "Saskia"]
+Use a for loop to loop over the array. Print every name individually.
+"""
+
+# list of names
+arr = ["Coen", "Casper", "Joshua", "Abdessamad", "Saskia"]
+
+# iterate trough the list of names,
+# it will print, "Coen" then "Capser" ect..
+for name in arr:
+    print(name)
